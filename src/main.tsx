9ccc1460c2a76455942eb79df53c8092704b28df
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-03-31 08:38:55 +0300

    Настройка роутинга

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ImportData from '~/pages/ImportData/ImportData.tsx';
+import EditingData from '~/pages/EditingData/EditingData.tsx';
+import Visualisation from '~/pages/Visualisation/Visualisation.tsx';
+import ExportData from '~/pages/ExportData/ExportData.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <ImportData/>,
+    },
+    {
+        path: '/editing',
+        element: <EditingData/>,
+    },
+    {
+        path: '/visualisation',
+        element: <Visualisation/>,
+    },
+    {
+        path: '/export',
+        element: <ExportData/>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+);
