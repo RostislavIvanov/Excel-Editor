828817b3f6eb2ac5b042874b5960303364c42492
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-05 11:55:49 +0300

    Реализация обновления данных таблицы

--- src/pages/EditingData/EditingData.tsx
+++ src/pages/EditingData/EditingData.tsx
@@ -1,38 +1,48 @@
 import DashboardLayout from '~/components/ui/DashboardLayout.tsx';
-import { useSelector } from 'react-redux';
-import { RootState } from '~/store/store.ts';
+import { useAppSelector } from '~/hooks/useAppSelector.ts';
+import { useAppDispatch } from '~/hooks/useAppDispatch.ts';
+import { updateData } from '~/store/tableData/tableDataSlice.ts';
+import { FC } from 'react';
 
-const EditingData = () => {
-    const tableData = useSelector((state: RootState) => state.tableDataReducer);
+const EditingData: FC = () => {
+    const { data } = useAppSelector(state => state.tableDataReducer);
+    const dispatch = useAppDispatch();
+    const handleCellChange = (value: string | null, row: number, col: number) => {
+        dispatch(updateData({ newValue: value, row, col }));
+    };
 
     return (
-        <div>
-            <DashboardLayout>
-                <div>
-                    {!!tableData.length &&
-                        <table>
-                            <thead>
-                            <tr>
-                                {Object.keys(tableData[0]).map((column, index) => (
-                                    <th className={'p-2 border border-black'} key={index}>{column}</th>
+        <DashboardLayout>
+            <div className={'overflow-auto container max-h-[100vh]'}>
+                {!!data.length &&
+                    <table>
+                        <thead>
+                        <tr>
+                            {Object.keys(data[0]).map((column, index) => (
+                                <th className={'p-2 border border-black'} key={index}>{column}</th>
+                            ))}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}>
+                                {Object.keys(data[0]).map((column, columnIndex) => (
+                                    <td className={'p-2 text-center border border-black'}
+                                        key={columnIndex}
+                                        contentEditable={true}
+                                        suppressContentEditableWarning={true} // Для подавления предупреждения
+                                        onBlur={(e) => handleCellChange(e.target.textContent, index, columnIndex)}
+                                    >
+                                        {item[column]}
+                                    </td>
                                 ))}
                             </tr>
-                            </thead>
-                            <tbody>
-                            {tableData.map((item, index) => (
-                                <tr key={index}>
-                                    {Object.keys(tableData[0]).map((column, columnIndex) => (
-                                        <td className={'p-2 text-center border border-black'}
-                                            key={columnIndex}>{item[column].toLocaleString()}</td>
-                                    ))}
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    }
-                </div>
-            </DashboardLayout>
-        </div>
+                        ))}
+                        </tbody>
+                    </table>
+                }
+            </div>
+        </DashboardLayout>
     );
 };
 
