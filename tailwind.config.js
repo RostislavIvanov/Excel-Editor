43192daf6710820d534460424fa7841441a0dfd0
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-06 10:32:35 +0300

    Дополнение темы

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lightGrey': '#eeeeee',
+        'borderGrey': 'rgb(156 163 175)'
+      },
+    },
   },
   plugins: [],
-}
+};
 
