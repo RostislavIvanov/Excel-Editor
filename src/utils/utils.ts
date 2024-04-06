821809bf4509936ea3aa234b536721894990327d
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-06 18:26:00 +0300

    Добавление функции получения типов значений ключей объекта

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,13 @@ export const countOccurrences = (arr: any[]) => {
         return acc;
     }, {});
 };
+
+export const getObjectKeyTypes = (obj: Record<string, number | string>): string[] => {
+    const types = [];
+    for (const key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            types.push(typeof obj[key]);
+        }
+    }
+    return types;
+};
