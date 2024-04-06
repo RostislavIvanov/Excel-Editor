c0dceb0bd31cd7447c13cadbd74296da19070a43
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-06 18:27:46 +0300

    Оформление таблицы

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { TableDataType } from '~/types/tableTypes.ts';
 import { useAppDispatch } from '~/hooks/useAppDispatch.ts';
 import { updateData } from '~/store/tableData/tableDataSlice.ts';
+import clsx from 'clsx';
 
 const ALPHABET = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
@@ -14,9 +15,13 @@ type TableProps = {
 const Table: FC<TableProps> = ({ data, columnTypes }) => {
     const dispatch = useAppDispatch();
     const keys = Object.keys(data[0]);
-
+    const [ chosenCol, setChosenCol] = useState<number | undefined>(undefined);
     const handleCellChange = (value: string | null, row: number, col: number) => {
         dispatch(updateData({ newValue: value, row, col }));
+    };
+
+    const handleChoosingColumn = (columnIndex: number) => {
+        setChosenCol(columnIndex);
     };
 
     const columnLettering = (): string[] => {
@@ -25,6 +30,7 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
 
     return (
         <table>
+
             <thead className={'sticky top-0'}>
             <tr>
                 <td className={'p-2 text-center bg-lightGrey border border-borderGrey cursor-default'}>
@@ -32,7 +38,12 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
                 </td>
                 {columnLettering().map((letter, index) =>
                     <th key={index}
-                        className={'font-normal p-2 border border-borderGrey text-center bg-lightGrey cursor-pointer'}>
+                        className={clsx('font-normal p-2 border border-borderGrey text-center cursor-pointer', {
+                            'text-white bg-darkBlue': index === chosenCol,
+                            'bg-lightGrey hover:bg-hoverGrey': index !== chosenCol,
+                        })}
+                        onClick={() => handleChoosingColumn(index)}
+                    >
                         {letter.toUpperCase()}
                     </th>,
                 )}
@@ -42,12 +53,17 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
                     1
                 </td>
                 {keys.map((column, index) => (
-                    <td className={'p-2 border border-borderGrey font-bold bg-lightGrey text-center whitespace-nowrap'} key={index}>
+                    <td className={clsx('p-2 border border-borderGrey font-bold bg-lightGrey text-center whitespace-nowrap', {
+                        'border-2 border-darkBlue': index === chosenCol,
+                        'text-textBlue': columnTypes[index] === 'number'
+                    })}
+                        key={index}>
                         {column}
                     </td>
                 ))}
             </tr>
             </thead>
+
             <tbody>
             {data.map((item, index) => (
                 <tr key={index}>
@@ -55,7 +71,10 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
                         {index + 2}
                     </th>
                     {keys.map((column, columnIndex) => (
-                        <td className={'p-2 text-center border border-borderGrey'}
+                        <td className={clsx('p-2 border-borderGrey text-center border', {
+                            'bg-lightBlue border-darkBlue': columnIndex === chosenCol,
+                            'text-textBlue': columnTypes[columnIndex] === 'number'
+                        })}
                             key={columnIndex}
                             contentEditable={true}
                             suppressContentEditableWarning={true} // Для подавления предупреждения
