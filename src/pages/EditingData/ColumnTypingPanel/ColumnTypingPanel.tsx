c421aefe727d07aa1658e6d40e6301442d0633f6
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-08 11:18:37 +0300

    Добавление панели выбора типа для колонки таблицы

---
+++ src/pages/EditingData/ColumnTypingPanel/ColumnTypingPanel.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { useAppSelector } from '~/hooks/useAppSelector.ts';
+import { useAppDispatch } from '~/hooks/useAppDispatch.ts';
+import { changeColumnType, updateColumn } from '~/store/tableData/tableDataSlice.ts';
+
+const ColumnTypingPanel: FC = () => {
+    const { data, chosenCol, columnTypes } = useAppSelector(state => state.tableDataReducer);
+    const [ selectedValue, setSelectedValue ] = useState(columnTypes[chosenCol]);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        setSelectedValue(columnTypes[chosenCol]);
+    }, [ chosenCol ]);
+
+    const handleDropdownChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        dispatch(changeColumnType(event.target.value));
+        setSelectedValue(event.target.value);
+        dispatch(updateColumn({ type: event.target.value, col: chosenCol }));
+    };
+
+    return (
+        <>
+            {chosenCol !== -1 &&
+                <>
+                    <div className={'text-2xl my-8 inline-block'}>
+                        Тип колонки {Object.keys(data[0])[chosenCol]}:
+                    </div>
+                    <div className={'inline-block ml-2'}>
+                        <select
+                            value={selectedValue}
+                            onChange={handleDropdownChange}
+                            className="py-2 px-4 border border-gray-600 rounded-[50px] focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
+                            name="typeSelector"
+                            id="typeSelector"
+                        >
+                            <option value="string">string</option>
+                            <option value="number">number</option>
+                            <option value="object">date</option>
+                        </select>
+                    </div>
+                </>
+            }
+        </>
+    )
+        ;
+};
+
+export default ColumnTypingPanel;
--- src/pages/EditingData/EditingData.tsx
+++ src/pages/EditingData/EditingData.tsx
@@ -2,12 +2,14 @@ import DashboardLayout from '~/components/ui/DashboardLayout.tsx';
 import { useAppSelector } from '~/hooks/useAppSelector.ts';
 import { FC } from 'react';
 import Table from '~/components/Table/Table.tsx';
+import ColumnTypingPanel from '~/pages/EditingData/ColumnTypingPanel/ColumnTypingPanel.tsx';
 
 const EditingData: FC = () => {
     const { data, columnTypes } = useAppSelector(state => state.tableDataReducer);
 
     return (
         <DashboardLayout>
+            <ColumnTypingPanel/>
             <div className={'overflow-auto container max-h-[100vh]'}>
                 {!!data.length &&
                     <Table data={data} columnTypes={columnTypes}/>
