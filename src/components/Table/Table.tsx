8a9a183201f791497b2c41f4ce420debd643e3ab
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-08 11:17:06 +0300

    Добавление логики выбора столбца и обработки цвета для даты

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,9 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { TableDataType } from '~/types/tableTypes.ts';
 import { useAppDispatch } from '~/hooks/useAppDispatch.ts';
-import { updateData } from '~/store/tableData/tableDataSlice.ts';
+import { chooseColumn, updateCell } from '~/store/tableData/tableDataSlice.ts';
 import clsx from 'clsx';
+import { useAppSelector } from '~/hooks/useAppSelector.ts';
 
 const ALPHABET = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
@@ -13,15 +14,15 @@ type TableProps = {
 };
 
 const Table: FC<TableProps> = ({ data, columnTypes }) => {
+    const { chosenCol } = useAppSelector(state => state.tableDataReducer);
     const dispatch = useAppDispatch();
     const keys = Object.keys(data[0]);
-    const [ chosenCol, setChosenCol ] = useState<number | undefined>(undefined);
     const handleCellChange = (value: string | null, row: number, col: number) => {
         dispatch(updateCell({ newValue: value, row, col }));
     };
 
     const handleChoosingColumn = (columnIndex: number) => {
-        setChosenCol(columnIndex);
+        dispatch(chooseColumn(columnIndex));
     };
 
     const columnLettering = (): string[] => {
@@ -56,6 +57,7 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
                     <td className={clsx('p-2 border border-borderGrey font-bold bg-lightGrey text-center whitespace-nowrap', {
                         'border-2 border-darkBlue': index === chosenCol,
                         'text-textBlue': columnTypes[index] === 'number',
+                        'text-orange-500': columnTypes[index] === 'object',
                     })}
                         key={index}>
                         {column}
@@ -74,6 +76,7 @@ const Table: FC<TableProps> = ({ data, columnTypes }) => {
                         <td className={clsx('p-2 border-borderGrey text-center border', {
                             'bg-lightBlue border-darkBlue': columnIndex === chosenCol,
                             'text-textBlue': columnTypes[columnIndex] === 'number',
+                            'text-orange-500': columnTypes[columnIndex] === 'object',
                         })}
                             key={columnIndex}
                             contentEditable={true}
