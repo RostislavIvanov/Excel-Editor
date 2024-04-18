0497ec8d935c99bc76264a7fe000891621f67511
Rostislav Ivanov <ros_ivanovaa@mail.ru>
2024-04-18 21:01:11 +0300

    Добавление новых компонентов диаграмм

--- src/pages/Visualisation/ChartSelect/ChartSelect.tsx
+++ src/pages/Visualisation/ChartSelect/ChartSelect.tsx
@@ -2,6 +2,9 @@ import { FC } from 'react';
 import { ChartType } from '~/pages/Visualisation/Visualisation.tsx';
 import PieCreate from '~/components/PieCreate/PieCreate.tsx';
 import LineChart from '~/components/LineChart/LineChart.tsx';
+import PolarAreaCreate from '~/components/PolarArea/PolarAreaCreate.tsx';
+import DonutChart from '~/components/DonutChart/DonutChart.tsx';
+import RadarChart from '~/components/RadarChart/RadarChart.tsx';
 
 type ChartSelectProps = {
     chart: ChartType;
@@ -15,6 +18,15 @@ const ChartSelect: FC<ChartSelectProps> = ({ chart }) => {
             }
             case 'lineChart': {
                 return <LineChart/>;
+            }
+            case 'polarArea': {
+                return <PolarAreaCreate/>;
+            }
+            case 'donutChart': {
+                return <DonutChart/>;
+            }
+            case 'radarChart': {
+                return <RadarChart/>;
             }
             default: {
                 return null;
